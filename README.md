454e7a7f59c62f0f535720d825c629c4419cc7a8
Joan Marc Riera Duocastella <joanmarcriera@gmail.com>
2018-03-26 18:55:03 +0100

    added stuff

--- README.md
+++ README.md
@@ -7,4 +7,18 @@ Script to enroll on gym classes
 I want to learn python to automate gym enrollment.
 https://booking.1life.co.uk/Connect/MRMLogin.aspx
 
+# How to
+
+## Create virtual env 
+
+virtualenv venv 
+source venv/bin/activate
+pip install -r requirements.txt
+
+## Config
+
+## Run 
+
+./enroll.py <user> <pass> "class"
+
 
---
+++ book.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+from selenium import webdriver
+import time
+
+web_user = "annaborrasm@gmail.com"
+web_pass = "1339"
+prefered_center = "Lord Butler Leisure"
+activities_to_enroll = ['Body Balance Tue 17:30','Body Step Tue 18:30','Body Combat Wed 17:45','Body Pump Thu 18:30','Pilates Thu 09:00','Body Attack Thu 09:30']
+
+options = webdriver.ChromeOptions()
+options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+options.add_argument('window-size=800x841')
+driver=webdriver.Chrome(chrome_options=options)
+driver.get("https://booking.1life.co.uk/Connect/MRMLogin.aspx")
+username = driver.find_element_by_id("ctl00_MainContent_InputLogin")
+password = driver.find_element_by_id("ctl00_MainContent_InputPassword")
+username.send_keys(web_user)
+password.send_keys(web_pass)
+driver.find_element_by_id("ctl00_MainContent_btnLogin").click()
+time.sleep(2)
+driver.find_element_by_xpath("//select[@id='ctl00_MainContent__advanceSearchUserControl_SitesSimple']/option[text()='Lord Butler Leisure']").click()
+
+time.sleep(3)
+
+search_box = driver.find_element_by_id("ctl00_ctl11_SearchTextBox")
+search_box.clear()
+search_box.send_keys("Body Step Tue 18:30")
+search_box.submit()
+
+
+
